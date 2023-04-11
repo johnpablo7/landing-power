06a0927a1f5ed80889e045204f6039b81325f719
John Pablo <john.davis.2015.03@gmail.com>
2023-04-11 11:30:48 -0500

    fix style

--- src/components/shared/Works.tsx
+++ src/components/shared/Works.tsx
@@ -3,9 +3,9 @@ import { worklist } from "../../data/workslist";
 
 export const Works = () => {
   return (
-    <div className="min-h-screen px-4 py-16 md:px-8 md:py-16 lg:p-20 2xl:py-32 2xl:px-28 gap-7">
+    <div className="min-h-screen px-4 pt-10 pb-16 md:px-8 md:py-16 lg:p-20 2xl:py-32 2xl:px-28 gap-7">
       <div className="mb-8">
-        <h1 className="text-[#1E1F4B] text-4xl leading-10 font-bold mb-7 tracking-[1px]">
+        <h1 className="text-[#1E1F4B] text-4xl leading-10 font-bold mb-2 md:mb-7 tracking-[1px]">
           We Create World-Class Digital Products
         </h1>
         <p className="text-[#2E3E5C] text-xl">
@@ -23,16 +23,16 @@ export const Works = () => {
           />
           <p className="text-[#2E3E5C]">App Design - June 20, 2022</p>
           <h3 className="text-[#1E1F4B] text-2xl font-bold">App Redesign</h3>
-          <p className="text-[#2E3E5C] text-xl md:tracking-[1px]">
+          <p className="text-[#2E3E5C] text-xl md:tracking-[1px] mb-3">
             By information about design the world to the best instructors, heatc
             helping By information about design the world to the best
             instructors, heatc helping
           </p>
         </NavLink>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-7">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4">
           {worklist.map((work) => (
-            <div key={work.id} className="flex flex-col gap-2">
+            <div key={work.id} className="flex flex-col gap-1">
               <NavLink to="/">
                 <img
                   src={work.img}
@@ -41,7 +41,9 @@ export const Works = () => {
                 />
               </NavLink>
               <p className="text-[#2E3E5C]">{work.date}</p>
-              <h3 className="text-[#1E1F4B] text-2xl font-bold">{work.name}</h3>
+              <h3 className="text-[#1E1F4B] text-2xl font-bold mb-2 md:mb-0">
+                {work.name}
+              </h3>
             </div>
           ))}
         </div>
